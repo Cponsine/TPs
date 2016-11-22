b3d5768ba87da94c0cfeb82e71bc866a2b862094
Coline <cponsine@plil.net>
2016-11-22 14:27:00 +0100

    commit 11/22/01

---
+++ tp1/BonjourPersonnalise.java
@@ -0,0 +1,6 @@
+public	class	BonjourPersonnalise {
+    /**ceci est un commentaire javadoc*/
+    public static void main(String[] args){
+          System.out.println("Bonjour "+args[0]);					
+    }
+}
